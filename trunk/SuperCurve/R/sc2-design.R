96b1fa2e1cda017cf841163df14e707ce689c184
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-05-10 08:05:05 +0000

    Reformatted code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/supercurve@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/SuperCurve/R/sc2-design.R
+++ trunk/SuperCurve/R/sc2-design.R
@@ -3,8 +3,9 @@
 ###
 
 
-RPPADesignParams <- function(steps=rep(0,1),
-                             series=factor(rep(0,1)),
+##-----------------------------------------------------------------------------
+RPPADesignParams <- function(steps=rep(0, 1),
+                             series=factor(rep(0, 1)),
                              grouping=c("byRow",
                                         "byCol",
                                         "bySample",
@@ -21,32 +22,30 @@ RPPADesignParams <- function(steps=rep(0,1),
         ordering=ordering,
         alias=alias,
         center=center,
-        controls=controls)                 
+        controls=controls)
 }
 
-if (FALSE) {
-.attachslot <- function(x) {
-    xname <- substitute(x)
-    sl <- names(getSlots(class(x)))
-    slotnames <- paste(sl, " <<- ", xname, "@", sl, sep = "")
-    for (i in slotnames) {
-        eval(parse(text = substitute(slotnames, list(slotnames = slotnames))))     
-    }
-} 
-}
-     
-                
+
+##-----------------------------------------------------------------------------
 RPPADesignFromParams <- function(raw, designparams) {
-  RPPADesign(raw, designparams@steps, designparams@series, 
-    designparams@grouping, designparams@ordering, 
-    designparams@alias, designparams@center, designparams@controls)
+    RPPADesign(raw,
+               designparams@steps,
+               designparams@series,
+               designparams@grouping,
+               designparams@ordering,
+               designparams@alias,
+               designparams@center,
+               designparams@controls)
 }
 
-# This is bad to have two constructors here since now this has to be kept in sync with PRRADesignParams
+
+##-----------------------------------------------------------------------------
+# This is bad to have two constructors here since now this has to be kept in
+# sync with PRRADesignParams
 # Only keep for backwards compatibility atm
 RPPADesign <- function(raw,
-                       steps=rep(0,1),
-                       series=factor(rep(0,1)),
+                       steps=rep(0, 1),
+                       series=factor(rep(0, 1)),
                        grouping=c("byRow",
                                   "byCol",
                                   "bySample",
@@ -56,172 +55,238 @@ RPPADesign <- function(raw,
                        alias=list(),
                        center=FALSE,
                        controls=list()) {
-  if (length(alias) < 1) {
-	
-    alias <- list(Alias=levels(raw@data$Sample), 
-      Sample=levels(factor(tolower(as.character(raw@data$Sample)))))
-  }
-  
-  if (inherits(raw, "RPPA")) raw <- raw@data
-  temp <- data.frame(raw[, c("Main.Row", "Main.Col", "Sub.Row", "Sub.Col", "Sample")])
-  if (length(steps) < 2 & length(series) < 2) {
-    grouping <- match.arg(grouping)
-    ordering <- match.arg(ordering)
-    steps <- rep(NA, nrow(raw))
-    series <- rep(NA, nrow(raw))
-    if (grouping == "byRow") {
-      series <- factor(paste("Series", temp$Main.Row, temp$Main.Col,
-                             temp$Sub.Row, sep='.'))
-      if (ordering == "increasing") {
-        steps <- temp$Sub.Col - 1
-      } else { # ordering == "decreasing"
-        steps <- max(temp$Sub.Col) - temp$Sub.Col
-      }
-    } else if (grouping == "byCol") {
-      series <- factor(paste("Series", temp$Main.Row, temp$Main.Col,
-                             temp$Sub.Col, sep='.'))
-      if (ordering == "increasing") {
-        steps <- temp$Sub.Row - 1
-      } else { # ordering == "decreasing"
-        steps <- max(temp$Sub.Row) - temp$Sub.Row
-      }
-    } else if (grouping == "bySample") {
-      series <- temp$Sample
-      for (sam in levels(temp$Sample)) {
-        where <- temp$Sample == sam
-        n <- sum(where)
-        if (ordering == "increasing") {
-          steps[where] <- -1 + (1:n)
-        } else { # ordering == "decreasing"
-          steps[where] <- n - (1:n)
-        }
-      } # end for(sam)
-    } else if (grouping == "blockSample") {
-      attach(temp)
-      series <- factor(paste(as.character(Sample), Main.Row, Main.Col, sep='.'))
-      detach()
-      for (sam in levels(series)) {
-        where <- series == sam
-        n <- sum(where)
-        if (ordering == "increasing") {
-          steps[where] <- -1 + (1:n)
-        } else { # ordering == "decreasing"
-          steps[where] <- n - (1:n)
-        }
-      } # end for(sam)      
-    } # end if grouping
-    
-    if (center) {
-	    for (ser in levels(series)) {
-	      where <- series == ser
-	      steps[where] <- steps[where] - median(steps[where])
-	    }
-	} else {
-	  # set top intensity (undiluted) spot to zero
-	    for (ser in levels(series)) {
-	      where <- series == ser
-	      steps[where] <- steps[where] - max(steps[where])
-	    }
-	}
-  
-  } else if (length(steps) < 2 | length(series) < 2) {
-    stop("You must supply both 'steps' and 'series' if you supply either one")
-  } else { # both series and steps supplied
-    if(length(steps) != nrow(temp) | length(series) != nrow(temp)) {
-      stop("lengths do not match")
+    ## :TODO: Need better checks on input values
+    if (length(alias) < 1) {
+        alias <- list(Alias=levels(raw@data$Sample),
+        Sample=levels(factor(tolower(as.character(raw@data$Sample)))))
     }
-    temp$Sample <- series # override sample names from file, with user supplied series names
-    # it is important to overrid so that users can specify controls with reference to their series names
-    
-  }# end if(is.null(steps))
-  
-  
-  if(any(is.na(steps))) warning("Some dilution steps have not been specified")
-  temp$Steps <- steps
-  temp$Series <- series
-  sampleMap <- as.vector(tapply(as.character(temp$Sample), list(series), function(x) x[[1]]))
-  sampleMap <- tolower(sampleMap)
-  names(sampleMap) <- levels(series)
-  
-  new("RPPADesign",
-      layout=temp,
-      alias=alias,
-      sampleMap=sampleMap,
-      controls=controls)
+
+    if (inherits(raw, "RPPA")) {
+        raw.df <- raw@data
+    }
+    temp <- data.frame(raw.df[, c("Main.Row",
+                               "Main.Col",
+                               "Sub.Row",
+                               "Sub.Col",
+                               "Sample")])
+    if (length(steps) < 2 && length(series) < 2) {
+        grouping <- match.arg(grouping)
+        ordering <- match.arg(ordering)
+        steps <- rep(NA, nrow(raw.df))
+        series <- rep(NA, nrow(raw.df))
+        if (grouping == "byRow") {
+            series <- factor(paste("Series",
+                                   temp$Main.Row,
+                                   temp$Main.Col,
+                                   temp$Sub.Row,
+                                   sep='.'))
+            steps <- if (ordering == "increasing") {
+                         temp$Sub.Col - 1
+                     } else {
+                         max(temp$Sub.Col) - temp$Sub.Col
+                     }
+        } else if (grouping == "byCol") {
+            series <- factor(paste("Series",
+                                   temp$Main.Row,
+                                   temp$Main.Col,
+                                   temp$Sub.Col,
+                                   sep='.'))
+            steps <- if (ordering == "increasing") {
+                         temp$Sub.Row - 1
+                     } else {
+                         max(temp$Sub.Row) - temp$Sub.Row
+                     }
+        } else if (grouping == "bySample") {
+            series <- temp$Sample
+            for (sam in levels(temp$Sample)) {
+                where <- temp$Sample == sam
+                n <- sum(where)
+                steps[where] <- if (ordering == "increasing") {
+                                    -1 + (1:n)
+                                } else {
+                                    n - (1:n)
+                                }
+            }
+        } else if (grouping == "blockSample") {
+            attach(temp)
+            series <- factor(paste(as.character(Sample),
+                                   Main.Row,
+                                   Main.Col,
+                                   sep='.'))
+            detach()
+            for (sam in levels(series)) {
+                where <- series == sam
+                n <- sum(where)
+                steps[where] <- if (ordering == "increasing") {
+                                    -1 + (1:n)
+                                } else {
+                                    n - (1:n)
+                                }
+            }
+        }
+
+        if (center) {
+            for (ser in levels(series)) {
+                where <- series == ser
+                steps[where] <- steps[where] - median(steps[where])
+            }
+        } else {
+            # set top intensity (undiluted) spot to zero
+            for (ser in levels(series)) {
+                where <- series == ser
+                steps[where] <- steps[where] - max(steps[where])
+            }
+        }
+
+    } else if (length(steps) < 2 || length(series) < 2) {
+        stop("You must supply both 'steps' and 'series' if you supply either one")
+    } else {
+        # both series and steps supplied
+        if (length(steps) != nrow(temp) || length(series) != nrow(temp)) {
+            stop("lengths do not match")
+        }
+        # override sample names from file, with user supplied series names
+        temp$Sample <- series
+        # it is important to override so that users can specify
+        # controls with reference to their series names
+
+    }
+
+    if (any(is.na(steps))) {
+        warning("Some dilution steps have not been specified")
+    }
+    temp$Steps <- steps
+    temp$Series <- series
+    sampleMap <- as.vector(tapply(as.character(temp$Sample),
+                                  list(series),
+                                  function(x) {
+                                      x[[1]]
+                                  }))
+    sampleMap <- tolower(sampleMap)
+    names(sampleMap) <- levels(series)
+
+    new("RPPADesign",
+        layout=temp,
+        alias=alias,
+        sampleMap=sampleMap,
+        controls=controls)
 }
 
 
+##-----------------------------------------------------------------------------
 # plot the series in an RPPA under a given design layout
 # see if the series make sense under this layout
-plotDesign.org <- function(rppa, design, measure='Mean.Total', main='') {
-	y <- rppa@data[, measure]
-	x <- design@layout$Steps
-	plot(c(min(x),max(x)), c(min(y), max(y)), type='n', main=paste(measure, "Intensity v.s. Dilution Step", main), 
-	  xlab = 'Dilution Step', ylab = 'Intensity')
-	series <- design@layout$Series
-	s <- seriesNames(design) # strip out control spots
-	rows <- length(s)
-	bow <- rainbow(rows)
-	for (i in 1:rows) {
-		lines(x = x[series == s[i]], y = y[series == s[i]], col=bow[i], type='b')
-	}
-}
-
-#########
-plotDesign <- function(rppa, design, measure='Mean.Total', main='') {
-        y <- rppa@data[, measure]
-        x <- design@layout$Steps
-
-      ########
-      ##The following were modified by Wenbin Liu:
-      # Sometimes there are many (e.g., hundreds of) appearances of 'control' in the
-      # Sample column and the original max(x) will mess up the plot.
-      #######
-               
-        is.ctrl<- .controlVector(design)  #get the indexes of the control spots.
-        par(mfrow=c(1,1)) #added by Wenbin Liu to avoid existing partitions of graphic device.
-        plot(c(min(x[!is.ctrl]),max(x[!is.ctrl])), c(min(y), max(y)), type='n', main=paste(measure, "Intensity v.s. Dilution Step", main), 
-          xlab = 'Dilution Step', ylab = 'Intensity')
-        series <- design@layout$Series
-        s <- seriesNames(design) # strip out control spots
-        rows <- length(s)
-        bow <- rainbow(rows)
-        for (i in 1:rows) {
-            lines(x = x[series == s[i]], y = y[series == s[i]], col=bow[i], type='b')
-        }
+plotDesign.org <- function(rppa,
+                           design,
+                           measure='Mean.Total',
+                           main='') {
+    y <- rppa@data[, measure]
+    x <- design@layout$Steps
+    plot(c(min(x), max(x)),
+         c(min(y), max(y)),
+         type='n',
+         main=paste(measure, "Intensity v.s. Dilution Step", main),
+         xlab='Dilution Step',
+         ylab='Intensity')
+    series <- design@layout$Series
+    s <- seriesNames(design) # strip out control spots
+    rows <- length(s)
+    bow <- rainbow(rows)
+    for (i in seq(1, rows)) {
+        lines(x=x[series == s[i]],
+              y=y[series == s[i]],
+              col=bow[i],
+              type='b')
+    }
 }
 
 
+##-----------------------------------------------------------------------------
+plotDesign <- function(rppa,
+                       design,
+                       measure='Mean.Total',
+                       main='') {
+    y <- rppa@data[, measure]
+    x <- design@layout$Steps
+
+    ########
+    ##The following were modified by Wenbin Liu:
+    # Sometimes there are many (e.g., hundreds of) appearances of 'control'
+    # in the Sample column and the original max(x) will mess up the plot.
+    #######
+
+    is.ctrl<- .controlVector(design)  # get the indexes of the control spots
+    par(mfrow=c(1, 1)) # avoid existing partitions of graphic device.
+    plot(c(min(x[!is.ctrl]), max(x[!is.ctrl])),
+         c(min(y), max(y)),
+         type='n',
+         main=paste(measure, "Intensity v.s. Dilution Step", main),
+         xlab='Dilution Step',
+         ylab='Intensity')
+    series <- design@layout$Series
+    s <- seriesNames(design) # strip out control spots
+    rows <- length(s)
+    bow <- rainbow(rows)
+    for (i in seq(1, rows)) {
+        lines(x=x[series == s[i]],
+              y=y[series == s[i]],
+              col=bow[i],
+              type='b')
+    }
+}
 
 
+##-----------------------------------------------------------------------------
 .controlVector <- function(design) {
-  sample <- as.character(design@layout$Sample)
-  temp <- rep(FALSE, length(unique(sample)))
-  names(temp) <- unique(sample)
-  temp[unlist(design@controls)] <- TRUE
-  temp[sample]
+    sample <- as.character(design@layout$Sample)
+    temp <- rep(FALSE, length(unique(sample)))
+    names(temp) <- unique(sample)
+    temp[unlist(design@controls)] <- TRUE
+    temp[sample]
 }
-  
+
+
+##-----------------------------------------------------------------------------
 seriesNames <- function(design) {
-  if (!inherits(design, "RPPADesign"))
-    stop("Invalid design class")
-  isControl <- .controlVector(design)
-  series <- as.character(design@layout$Series[!isControl])
-  unique(series)
+    if (!inherits(design, "RPPADesign")) {
+        stop("Invalid design class")
+    }
+    isControl <- .controlVector(design)
+    series <- as.character(design@layout$Series[!isControl])
+    unique(series)
 }
 
+
+##-----------------------------------------------------------------------------
 getSteps <- function(design) {
-  isControl <- .controlVector(design)
-  design@layout$Steps[!isControl]
+    isControl <- .controlVector(design)
+    design@layout$Steps[!isControl]
 }
 
+
+##-----------------------------------------------------------------------------
 setMethod("names", "RPPADesign",
           function(x) {
-  isControl <- .controlVector(x)
-  as.character(x@layout$Series[!isControl])
+    isControl <- .controlVector(x)
+    as.character(x@layout$Series[!isControl])
 })
 
-if(FALSE) {
+
+
+if (FALSE) {
+    .attachslot <- function(x) {
+        xname <- substitute(x)
+        sl <- names(getSlots(class(x)))
+        slotnames <- paste(sl, " <<- ", xname, "@", sl, sep="")
+        for (i in slotnames) {
+            eval(parse(text=substitute(slotnames,
+                                       list(slotnames=slotnames))))
+        }
+    }
+}
+
+
+if (FALSE) {
   path <- "../inst/rppaTumorData"
   erk2 <- RPPA("ERK2.txt", path=path)
   design <- RPPADesign(erk2, grouping="blockSample", center=TRUE)
