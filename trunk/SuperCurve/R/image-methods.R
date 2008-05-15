75ac0d5193de00ecb82a382eca2d1b077deb4ba2
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-05-15 21:11:14 +0000

    modified RPPA's image method to use updated code for colorbar. removed S3 image.RPPAFit routine. and what update would be complete without some reformatting changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/supercurve@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/SuperCurve/R/image-methods.R
+++ trunk/SuperCurve/R/image-methods.R
@@ -41,75 +41,112 @@ image.RPPA <- function(x,
     mx <- max(data.df$Main.Col) * max(data.df$Sub.Col)
     yspot <- 1+my-(max(data.df$Sub.Row)*(data.df$Main.Row-1) + data.df$Sub.Row)
     xspot <- max(data.df$Sub.Col)*(data.df$Main.Col-1) + data.df$Sub.Col
-    geo <- tapply(data.df[,measure], list(xspot, yspot), mean)
+    geo <- tapply(data.df[, measure],
+                  list(xspot, yspot),
+                  mean)
     if (colorbar) {
-        layout(matrix(c(2, 1), nrow=1), widths=c(10,1))
-        opar <- par(mai=c(1, 0.5, 1, 0.1))
-        on.exit(par(opar))
-        on.exit(layout(1))
+        ## get the size of the plotting region in relative units
+        startPlt <- par()$plt
+
+        ## We're only going to partition things on the x-axis, so only
+        ## the first 2 coordinates are of interest. Define the boundaries
+        ## for the two panels so that a 10/1 width ratio is attained.
+        imagePlt    <- startPlt
+        colorbarPlt <- startPlt
+        startWidth  <- startPlt[2] - startPlt[1]
+
+        imagePlt[2]    <- startPlt[1] + (10/12)*startWidth
+        colorbarPlt[1] <- startPlt[2] - ( 1/12)*startWidth
+
+        ## draw the colorbar
+        par(plt=colorbarPlt)
         image(1,
               seq(min(geo, na.rm=TRUE),
                   max(geo, na.rm=TRUE),
                   length=256),
               matrix(1:256, nrow=1),
               col=col,
-              xaxt='n',
-              xlab='',
-              ylab='')
+              xaxt="n",
+              xlab="",
+              yaxt="n",
+              ylab="")
+        axis(4) # put labeling at right
+        box()
+
+        ## set things up to draw the main image and
+        ## revert back for the next figure
+        par(plt=imagePlt, new=TRUE)
+        on.exit(par(plt=startPlt))
     }
-    image(1:mx, 1:my, geo, main=main, col=col, ...)
-    abline(h = 0.5 + seq(0, my, length=1+max(data.df$Main.Row)))
-    abline(v = 0.5 + seq(0, mx, length=1+max(data.df$Main.Col)))
+    image(1:mx,
+          1:my,
+          geo,
+          col=col,
+          main=main,
+          ...)
+    abline(h=(0.5 + seq(0, my, length=1+max(data.df$Main.Row))))
+    abline(v=(0.5 + seq(0, mx, length=1+max(data.df$Main.Col))))
     invisible(x)
 }
 
-setMethod("image", "RPPA", image.RPPA)
+## :TODO: Figure out how to combine with above and remove S3 definition
+setMethod("image", "RPPA",
+          image.RPPA)
 
 
 ##-----------------------------------------------------------------------------
 setMethod("image", "RPPADesign",
-          function(x, ...) {
-    # figure out how to make "geographic" pictures
+          function(x,
+                   ...) {
+    ## figure out how to make "geographic" pictures
     data.df <- x@layout
     my <- max(data.df$Main.Row) * max(data.df$Sub.Row)
     mx <- max(data.df$Main.Col) * max(data.df$Sub.Col)
     yspot <- 1+my-(max(data.df$Sub.Row)*(data.df$Main.Row-1) + data.df$Sub.Row)
     xspot <- max(data.df$Sub.Col)*(data.df$Main.Col-1) + data.df$Sub.Col
-    geo.steps <- tapply(data.df$Steps, list(xspot, yspot), mean)
-    image(1:mx, 1:my, geo.steps, ...)
-    abline(h = 0.5 + seq(0, my, length=1+max(data.df$Main.Row)))
-    abline(v = 0.5 + seq(0, mx, length=1+max(data.df$Main.Col)))
+    geo.steps <- tapply(data.df$Steps,
+                        list(xspot, yspot),
+                        mean)
+    image(1:mx,
+          1:my,
+          geo.steps,
+          ...)
+    abline(h=(0.5 + seq(0, my, length=1+max(data.df$Main.Row))))
+    abline(v=(0.5 + seq(0, mx, length=1+max(data.df$Main.Col))))
     invisible(geo.steps)
 })
 
 
 ##-----------------------------------------------------------------------------
-# The image function gives a geographic plot of some measure computed from
-# the fit. Default is to image the (raw) residuals, with options for other
-# forms of the residuals or for the fitted concentrations (X) or intensities (Y).
-
-image.RPPAFit <- function(x,
-                          measure=c("Residuals",
-                                    "ResidualsR2",
-                                    "StdRes",
-                                    "X",
-                                    "Y"),
-                          ...) {
+## Provides a geographic plot of some measure computed from the fit.
+## Default is to image the (raw) residuals, with options for other forms
+## of the residuals or for the fitted concentrations (X) or intensities (Y).
+setMethod("image", "RPPAFit",
+          function(x,
+                   measure=c("Residuals",
+                             "ResidualsR2",
+                             "StdRes",
+                             "X",
+                             "Y"),
+                   ...) {
     ## Check arguments
     measure <- match.arg(measure)
 
     ## Begin processing
     rppa <- x@rppa
+    ## :TBD: Why not just requested measure? Isn't doing rest unnecessary?
     rppa@data$Residuals <- resid(x)
     rppa@data$StdRes <- resid(x, "standardized")
     rppa@data$ResidualsR2 <- resid(x, "r2")
-    rppa@data$X = fitted(x, "X")
-    rppa@data$Y = fitted(x, "Y")
-    image.RPPA(rppa, measure=measure, ...)
+    rppa@data$X <- fitted(x, "X")
+    rppa@data$Y <- fitted(x, "Y")
+
+    ## :TBD: What should axis labels be?
+    ## :TBD: Shouldn't main reference file as well?
+    image(rppa,
+          measure=measure,
+          ...)
 
     invisible(x)
-}
-
-setMethod("image", "RPPAFit",
-          image.RPPAFit)
+})
 
