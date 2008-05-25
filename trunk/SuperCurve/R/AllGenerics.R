439c917c3de1ccdbb232c9223ded9ff7c48b4008
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-05-25 06:44:12 +0000

    reordered generic methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/supercurve@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/SuperCurve/R/AllGenerics.R
+++ trunk/SuperCurve/R/AllGenerics.R
@@ -2,82 +2,85 @@
 ### ALLGENERICS.R
 ###
 
-###############################
+
+##
 ## S3 or non-generics converted to S4
+##
 
-# defined for: RPPA, RPPADesign, RPPAFit
-if (!isGeneric("summary")) {
-    setGeneric("summary",
-               function(object, ...) standardGeneric("summary"))
-}
-
-# defined for: RPPA, RPPAFit. RPPADesign
-if (!isGeneric("image")) {
-    setGeneric("image",
-               function(x, ...) standardGeneric("image"))
-}
-
-# defined for: RPPAFit
-# Why not RPPADEsign, since there is a [expletive] plotDesign function?
-if (!isGeneric("plot")) {
-    setGeneric("plot",
-               function(x, y, ...) standardGeneric("plot"))
-}
-
-# defined for: RPPAFit
-if (!isGeneric("hist")) {
-    setGeneric("hist",
-               function(x, ...) standardGeneric("hist"))
-}
-
-# defined for: nothing. so why is it here?
+## defined for: nothing. so why is it here?
 if (!isGeneric("coef")) {
     setGeneric("coef",
                function(object, ...) standardGeneric("coef"))
 }
 
-# defined for: nothing. so why is it here?
+## defined for: nothing. so why is it here?
 if (!isGeneric("coefficients")) {
     setGeneric("coefficients",
                function(object, ...) standardGeneric("coefficients"))
 }
 
-# defined for: LoessFitClass, CobsFitClass, LogisticFitClass, RPPAFit
+## defined for: LoessFitClass, CobsFitClass, LogisticFitClass, RPPAFit
 if (!isGeneric("fitted")) {
     setGeneric("fitted",
                function(object, ...) standardGeneric("fitted"))
 }
   
-# defined for: RPPAFit
+## defined for: RPPAFit
+if (!isGeneric("hist")) {
+    setGeneric("hist",
+               function(x, ...) standardGeneric("hist"))
+}
+
+## defined for: RPPA, RPPAFit. RPPADesign
+if (!isGeneric("image")) {
+    setGeneric("image",
+               function(x, ...) standardGeneric("image"))
+}
+
+## defined for: RPPAFit
+## :KRC: Why not RPPADEsign, since there is a [expletive] plotDesign function?
+if (!isGeneric("plot")) {
+    setGeneric("plot",
+               function(x, y, ...) standardGeneric("plot"))
+}
+
+## defined for: RPPAFit
 if (!isGeneric("resid")) {
     setGeneric("resid",
                function(object, ...) standardGeneric("resid"))
 }
 
-# defined for: RPPAFit
+## defined for: RPPAFit
 if (!isGeneric("residuals")) {
     setGeneric("residuals",
                function(object, ...) standardGeneric("residuals"))
 }
 
-###############################
-## Brand New Generic Functions
-
-# defined for: FitClass, LoessFitClass, CobsFitClass, LogisticFitClass
-if (!isGeneric("trimConc")) {
-    setGeneric("trimConc",
-               function(object, ...) standardGeneric("trimConc"))
+## defined for: RPPA, RPPADesign, RPPAFit
+if (!isGeneric("summary")) {
+    setGeneric("summary",
+               function(object, ...) standardGeneric("summary"))
 }
 
-# defined for: FitClass, LoessFitClass, CobsFitClass, LogisticFitClass
+##
+## Brand new generics
+##
+
+## defined for: FitClass, LoessFitClass, CobsFitClass, LogisticFitClass
 if (!isGeneric("fitSeries")) {
     setGeneric("fitSeries",
                function(object, ...) standardGeneric("fitSeries"))
 }
 
-# defined for: FitClass, LoessFitClass, CobsFitClass, LogisticFitClass
+## defined for: FitClass, LoessFitClass, CobsFitClass, LogisticFitClass
 if (!isGeneric("fitSlide")) {
     setGeneric("fitSlide",
                function(object, ...) standardGeneric("fitSlide"))
+}
+
+## defined for: FitClass, LoessFitClass, CobsFitClass, LogisticFitClass
+if (!isGeneric("trimConc")) {
+    setGeneric("trimConc",
+               function(object, ...) standardGeneric("trimConc"))
 }
 
