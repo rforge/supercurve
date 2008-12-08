2dccf50d091fe41b8378b5b4b60efc4ecd26711a
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-08 17:04:42 +0000

    First shot at data-driven solution for normalize() method. All provided normalization methods now register themselves for subsequent (internal) invocation via keys. Several administrative methods were added to handle store/fetch/query of registration of these normalization methods. Attributes of normalization processing that occurred are attached to the results for tracking.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/supercurve@281 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/SuperCurve/R/sc6-normalize.R
+++ trunk/SuperCurve/R/sc6-normalize.R
@@ -1,66 +1,76 @@
 ###
-### NORMALIZE.R
+### NORMALIZE.R - Used after RPPAFit to remove sample effect
 ###
 
 
+##
+## Module Variables
+##
+.NormEnv <- new.env(hash=TRUE)                   # Private environment
+
+
+##
+## Private Methods
+##
+
 ##-----------------------------------------------------------------------------
-## Performs normalization for sample loading after quantification.
-## It has two required input values: 1) the data matrix with samples
-## in the rows and proteins in the columns. 2) the method of sample
-## loading normalization:
-##   median - the sample median (row median) is subtracted from
-##            each sample
-##   house  - housekeeping normalization. The median of a housekeeping
-##            protein or set of housekeeping proteins are used. The
-##            name of the protein(s) to be used must also be supplied.
-##   vs     - variable slope normalization. Here the sample median
-##            is used along with a multiplicate gamma
+## Returns private environment for storing registered normalization methods
+normenv <- function() {
+    return(.NormEnv)
+}
 
-## :KRC: Should be called something different; there are "normalize"
-## functions for every technology in the universe. The name should at
-## least include "rppa" somewhere.
 
-## :KRC: Want to "register" normalization methods, so having a "method"
-## argument sort of defeats that idea. Those are three different
-## functions that should each be registered.
-normalize <- function(concs,
-                      method=c("median", "house", "vs"),
-                      protein=NULL) {
-    ## Check arguments
-    if (is.RPPASet(concs)) {
-        ## Assemble matrix of concentrations from all fits in object
-        concs <- .fitSlot(concs, "concentrations")
-        ## :FIXME: Unfortunately, this has columns of filenames, not antibodies
-        ## Information available in 'proteinAssay.tsv' but no path here...
-    }
-    if (!(is.matrix(concs) || is.data.frame(concs))) {
-        stop(sprintf("argument %s must be matrix-like",
-                     sQuote("concs")))
-    }
+##-----------------------------------------------------------------------------
+## Normalization method. Sample median is subtracted from each sample.
+normalize.median <- function(concs,
+                             rowMedian) {
+    stopifnot(is.matrix(concs) || is.data.frame(concs))
+    stopifnot(is.numeric(rowMedian))
 
-    method <- match.arg(method)
+    return(normconcs <- sweep(concs, 1, rowMedian, FUN="-"))
+}
 
-    if (method == "house") {
-        if (!is.character(protein)) {
-            stop(sprintf("argument %s must be character",
-                         sQuote("protein")))
-        } else if (!all(protein %in% colnames(concs))) {
-            missingNames <- protein[!protein %in% colnames(concs)]
-            stop(sprintf(ngettext(length(missingNames),
-                                  "argument %s specifies invalid %s column name: %s",
-                                  "argument %s specifies invalid %s column names: %s"),
-                         sQuote("protein"),
-                         sQuote("concs"),
-                         paste(missingNames, collapse=", ")))
-        }
-    } else {
-        if (!is.null(protein)) {
-            warning(sprintf("argument %s ignored when using method=%s",
-                            sQuote("protein"), dQuote(method)))
-        }
+
+##-----------------------------------------------------------------------------
+## Normalization method. Median of set of housekeeping proteins is subtracted
+## from each sample.
+normalize.house <- function(concs,
+                            protein) {
+    stopifnot(is.matrix(concs) || is.data.frame(concs))
+    stopifnot(is.character(protein) && length(protein) >= 1)
+
+    if (!all(protein %in% colnames(concs))) {
+        missingNames <- protein[!protein %in% colnames(concs)]
+        stop(sprintf(ngettext(length(missingNames),
+                              "argument %s specifies invalid %s column name: %s",
+                              "argument %s specifies invalid %s column names: %s"),
+                     sQuote("protein"),
+                     sQuote("concs"),
+                     paste(missingNames, collapse=", ")))
     }
 
+    houseMedian <- apply(as.matrix(concs[, protein]),
+                         1,
+                         median,
+                         na.rm=TRUE)
+    normconcs <- sweep(concs, 1, houseMedian, FUN="-")
+    ## Store method-specific info in "normalization" attribute
+    attr(normconcs, "normalization") <- list(protein=protein,
+                                             houseMedian=houseMedian)
 
+    return(normconcs)
+}
+
+
+##-----------------------------------------------------------------------------
+## Normalization method (variable slope). Sample median is subtracted from
+## each sample after applying multiplicative gamma.
+normalize.vs <- function(concs,
+                         rowMedian) {
+    stopifnot(is.matrix(concs) || is.data.frame(concs))
+    stopifnot(is.numeric(rowMedian))
+
+    ##-------------------------------------------------------------------------
     ## Estimates the multiplicative gamma terms from variable slope
     ## normalization. It takes as input the data matrix (with samples in
     ## the rows and proteins in the columns). It is assumed that this matrix
@@ -80,21 +90,21 @@ normalize <- function(concs,
                 n <- length(a)
                 tt <- r * sqrt((n-2) / (1-r^2))
                 chk <- pt(tt, n-2, lower.tail=FALSE)
-                if (chk < .05) {
+                if (chk < 0.05) {
                     eig <- eigen(var(cbind(Xhat[, i], Xhat[, j]), na.rm=TRUE))
                     tmp <- (-1) * eig$vectors[1, 2] / eig$vectors[2, 2]
                     gamma[i, j] <- tmp
                 }
             }
         }
-        gamma[gamma<=0] <- 1
+        gamma[gamma <= 0] <- 1
         upper <- upper.tri(gamma)
-        ind <- which(upper, arr.ind=TRUE)
+        ind.upper <- which(upper, arr.ind=TRUE)
 
-        design <- matrix(0, ncol=nCol, nrow=nrow(ind))
-        for (i in seq_len(nrow(ind))) {
-            design[i, ind[i, 1]] <- -1
-            design[i, ind[i, 2]] <- 1
+        design <- matrix(0, ncol=nCol, nrow=nrow(ind.upper))
+        for (i in seq_len(nrow(ind.upper))) {
+            design[i, ind.upper[i, 1]] <- -1
+            design[i, ind.upper[i, 2]] <- 1
         }
 
         loggamma <- log(gamma[upper])
@@ -104,41 +114,143 @@ normalize <- function(concs,
         lestimateMean <- qr.solve(nonsingular, c(0, loggamma))
 
         estimate1 <- exp(lestimateMean)
-        val <- estimate1
     }
 
+
+    gamma <- estimateGamma(concs)
+    temp <- sweep(concs, 2, gamma, FUN="/")
+
+    normconcs <- sweep(temp, 1, rowMedian, FUN="-")
+    ## Store method-specific info in "normalization" attribute
+    attr(normconcs, "normalization") <- list(gamma=gamma)
+
+    return(normconcs)
+}
+
+
+##
+## Public Methods
+##
+
+##-----------------------------------------------------------------------------
+## Returns normalization method associated with key for invocation.
+getRegisteredNormalizationMethod <- function(key) {
+    return(method <- getRegisteredMethod(key, envir=normenv())$method)
+}
+
+
+##-----------------------------------------------------------------------------
+## Returns label associated with key for display by user interface.
+getRegisteredNormalizationMethodLabel <- function(key) {
+    return(ui.label <- getRegisteredMethod(key, envir=normenv())$ui.label)
+}
+
+
+##-----------------------------------------------------------------------------
+## Returns vector containing "keys" for all registered normalization methods.
+getRegisteredNormalizationMethodKeys <- function() {
+    keys <- getRegisteredMethodKeys(envir=normenv())
+    if (length(keys) == 0) {
+        stop("no registered normalization methods exist")
+    }
+
+    return(keys)
+}
+
+
+##-----------------------------------------------------------------------------
+## Registers specific normalization method for use by normalize() method.
+registerNormalizationMethod <- function(key,
+                                        method,
+                                        ui.label=names(key)) {
+    if (is.null(ui.label)) {
+        ui.label <- key
+    }
+    ui.label <- as.character(ui.label)[1]
+
+    registerMethod(key, method, ui.label=ui.label, envir=normenv())
+}
+
+
+##-----------------------------------------------------------------------------
+## Performs normalization for sample loading after quantification.
+## It has two required input values:
+##   1) the data matrix with samples in the rows and proteins in the columns.
+##   2) the name of the method of sample loading normalization. This argument
+##      may be augmented with user-provided normalization methods.
+##      Package-provided values are:
+##
+##      median - the sample median (row median) is subtracted from each sample
+##      house  - housekeeping normalization. The median of a housekeeping
+##               protein or set of housekeeping proteins are used. The
+##               name of the protein(s) to be used must be supplied as
+##               a named argument to this method.
+##      vs     - variable slope normalization. Here the sample median
+##               is used along with a multiplicate gamma.
+##
+
+## :KRC: Should be called something different; there are "normalize"
+## functions for every technology in the universe. The name should at
+## least include "rppa" somewhere.
+
+normalize <- function(concs,
+                      method=getRegisteredNormalizationMethodKeys(),
+                      ...) {
+    ## Check arguments
+    if (is.RPPASet(concs)) {
+        ## Assemble matrix of concentrations from all fits in object
+        concs <- .fitSlot(concs, "concentrations")
+        ## :FIXME: Unfortunately, this has columns of filenames, not antibodies
+        ## Information available in 'proteinAssay.tsv' but no path here...
+    }
+
+    if (!(is.matrix(concs) || is.data.frame(concs))) {
+        stop(sprintf("argument %s must be matrix-like",
+                     sQuote("concs")))
+    }
+
+    if (!is.character(method)) {
+        stop(sprintf("argument %s must be character",
+                     sQuote("method")))
+    } else if (!(length(method) == 1)) {
+        stop(sprintf("argument %s must be of length 1",
+                     sQuote("method")))
+    } else if (!(nchar(method) >= 1)) {
+        stop(sprintf("argument %s must not be empty string", 
+                     sQuote("method")))
+    }
+
+    methodLabel <- method <- match.arg(method)
 
     ## Begin processsing
     rowMedian <- apply(concs, 1, median, na.rm=TRUE)
     colMedian <- apply(concs, 2, median, na.rm=TRUE)
 
-    concs <- sweep(concs, 2, colMedian)
-    gamma <- NULL
-    houseMedian <- NULL
+    method <- getRegisteredNormalizationMethod(method)
+    methodArgs <- alist(concs=sweep(concs, 2, colMedian, FUN="-"),
+                        rowMedian=rowMedian,
+                        ...)
+    if (!("rowMedian" %in% names(formals(method)))) {
+        ## Remove from argument list since not used
+        methodArgs$rowMedian <- NULL
+    }
 
-    dataNorm <- switch(EXPR=method,
-                       median = sweep(concs, 1, rowMedian),
-                       house  = {
-                                    ## housekeeping normalization
-                                    houseMedian <- apply(as.matrix(concs[, protein]),
-                                                         1,
-                                                         median,
-                                                         na.rm=TRUE)
-                                    sweep(concs, 1, houseMedian, "-")
-                                },
-                       vs     = {
-                                    ## variable slope normalization
-                                    gamma <- estimateGamma(concs)
-                                    temp <- sweep(concs, 2, gamma, "/")
-                                    sweep(temp, 1, rowMedian, "-")
-                                },
-                       stop(sprintf("unrecognized normalization method %s",
-                                    sQuote(method))))
+    normconcs <- do.call(method, methodArgs)
 
-    list(dataNorm=dataNorm,
-         rowMedian=rowMedian,
-         colMedian=colMedian,
-         gamma=gamma,
-         houseMedian=houseMedian)
+    ## Store processing info in "normalization" attribute
+    attr(normconcs, "normalization") <- c(list(method=methodLabel,
+                                               rowMedian=rowMedian,
+                                               colMedian=colMedian),
+                                          attr(normconcs, "normalization"))
+
+    return(normconcs)
 }
+
+
+##
+## Initialization
+##
+registerNormalizationMethod("median", normalize.median)
+registerNormalizationMethod("house", normalize.house, "Housekeeping")
+registerNormalizationMethod("vs", normalize.vs, "Variable Slope")
 
