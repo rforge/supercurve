b9a2e66f295dd262fb9ec36e5caa7b702a675090
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-08 18:08:42 +0000

    Generic registration routines used by data-driven portions of code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/supercurve@284 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/SuperCurve/R/registration.R
@@ -0,0 +1,147 @@
+###
+### REGISTRATION.R - Generic registration routines
+###
+
+
+##
+## Private Methods
+##
+
+##-----------------------------------------------------------------------------
+.validate.classname <- function(classname) {
+    if (!is.character(classname)) {
+        stop(sprintf("argument %s must be character",
+                     sQuote("classname")))
+    } else if (!(length(classname) == 1)) {
+        stop(sprintf("argument %s must be of length 1",
+                     sQuote("classname")))
+    } else if (!(nchar(classname) >= 1)) {
+        stop(sprintf("argument %s must not be empty string",
+                     sQuote("classname")))
+    }
+}
+
+
+##-----------------------------------------------------------------------------
+.validate.envir <- function(envir) {
+    if (!is.environment(envir)) {
+        stop(sprintf("argument %s must be environment",
+                     sQuote("envir")))
+    }
+}
+
+
+##-----------------------------------------------------------------------------
+.validate.key <- function(key) {
+    if (!is.character(key)) {
+        stop(sprintf("argument %s must be character",
+                     sQuote("key")))
+    } else if (!(length(key) == 1)) {
+        stop(sprintf("argument %s must be of length 1",
+                     sQuote("key")))
+    } else if (!(nchar(key) >= 1)) {
+        stop(sprintf("argument %s must not be empty string",
+                     sQuote("key")))
+    }
+}
+
+
+##-----------------------------------------------------------------------------
+.validate.method <- function(method) {
+    if (!is.function(method)) {
+        stop(sprintf("argument %s must be function",
+                     sQuote("method")))
+    }
+}
+
+
+##
+## Package Methods
+##
+
+##-----------------------------------------------------------------------------
+## Returns vector containing "keys" for all registered objects.
+getRegisteredObjectKeys <- function(envir) {
+    ## Check arguments
+    .validate.envir(envir)
+
+    ## Begin processsing
+    return(keys <- ls(envir=envir))
+}
+
+getRegisteredMethodKeys <- getRegisteredObjectKeys
+
+
+##-----------------------------------------------------------------------------
+## Returns list associated with key. List will contain named arguments provided
+## upon registration.
+getRegisteredObject <- function(key,
+                                envir,
+                                objtype=c("method",
+                                          "class",
+                                          "classname")) {
+    ## Check arguments
+    .validate.key(key)
+    .validate.envir(envir)
+    objtype <- match.arg(objtype)
+
+    ## Begin processsing
+    if (!exists(key, envir=envir)) {
+        stop(sprintf("no registered %s associated with argument %s (%s)",
+                     objtype,
+                     sQuote(key),
+                     key))
+    }
+
+    return(get(key, envir=envir))
+}
+
+
+##-----------------------------------------------------------------------------
+## Returns list associated with key. List will contain method as well as any
+## other named arguments provided upon registration. 
+getRegisteredMethod <- function(key,
+                                envir) {
+    getRegisteredObject(key, envir, "method")
+}
+
+
+##-----------------------------------------------------------------------------
+## Registers classname (and optionally other named arguments) by association to
+## key.
+registerClassname <- function(key,
+                              classname,
+                              ...,
+                              envir) {
+    ## Check arguments
+    .validate.key(key)
+    .validate.classname(classname)
+    .validate.envir(envir)
+
+    ## Begin processsing
+    dots <- list(...)
+    registerable <- c(list(classname=classname),
+                      namedArgs <- dots[names(dots) != ""])
+    assign(key, registerable, envir=envir)
+}
+
+
+##-----------------------------------------------------------------------------
+## Registers method (and optionally other named arguments) by association to
+## key.
+registerMethod <- function(key,
+                           method,
+                           ...,
+                           envir) {
+    ## Check arguments
+    .validate.key(key)
+    .validate.method(method)
+    .validate.envir(envir)
+
+    ## Begin processsing
+    dots <- list(...)
+    registerable <- c(list(method=method),
+                      namedArgs <- dots[names(dots) != ""])
+    assign(key, registerable, envir=envir)
+}
+
