f2b21edfda951b1955b866ba60539e396c0f3d09
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-10 23:26:59 +0000

    Removed .onload() method (now handled by 'tclish' package)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/supercurve@468 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/plrdev-scui-1.9/SuperCurveGUI/R/zzz.R
+++ branches/plrdev-scui-1.9/SuperCurveGUI/R/zzz.R
@@ -34,17 +34,3 @@
     }
 }
 
-
-##-----------------------------------------------------------------------------
-## Initialize Tcl resource database
-.onLoad <- function(libname, pkgname) {
-    optiondb_add("*Notebook.borderWidth", 2, "widgetDefault")
-    optiondb_add("*Notebook.relief", "sunken", "widgetDefault")
-    optiondb_add("*Tabnotebook.Canvas.background", "#666666", "widgetDefault")
-    optiondb_add("*Tabnotebook.margin", 6, "widgetDefault")
-    optiondb_add("*Tabnotebook.tabColor", "#a6a6a6", "widgetDefault")
-    optiondb_add("*Tabnotebook.activeTabColor", "#d9d9d9", "widgetDefault")
-    optiondb_add("*Tabnotebook.tabFont",
-                 "-*-helvetica-bold-r-normal--*-120-*", "widgetDefault")
-}
-
