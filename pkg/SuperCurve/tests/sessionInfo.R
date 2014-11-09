721a1f75b421973bf7ad348f930b88f387259c2f
proebuck <proebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-09 22:31:07 +0000

    Modified to load all suggested packages before running sessionInfo().
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/supercurve@919 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SuperCurve/tests/sessionInfo.R
+++ pkg/SuperCurve/tests/sessionInfo.R
@@ -4,7 +4,16 @@
 
 
 options(warn=1)
-library(SuperCurve)
+pkgname <- "SuperCurve"
+suppressPackageStartupMessages(library(pkgname, character.only=TRUE))
+
+## Load packages for all possible processing options
+suggested <- unlist(strsplit(packageDescription(pkgname)$Suggests, ', '))
+invisible(sapply(suggested,
+                 function(pkgname) {
+                     suppressPackageStartupMessages(
+                         require(pkgname, character.only=TRUE))
+                 }))
 
 show(sessionInfo())
 
