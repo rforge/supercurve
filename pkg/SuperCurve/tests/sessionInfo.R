5061f037cc2e66e36f499a32df5626d736c4b6c1
proebuck <proebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-24 18:08:49 +0000

    Updated test to handle multi-line metadata.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/supercurve@954 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SuperCurve/tests/sessionInfo.R
+++ pkg/SuperCurve/tests/sessionInfo.R
@@ -7,9 +7,21 @@ options(warn=1)
 pkgname <- "SuperCurve"
 suppressPackageStartupMessages(library(pkgname, character.only=TRUE))
 
+
+##-----------------------------------------------------------------------------
+## Returns character of package names corresponding to "Suggests:" field
+suggestedPackages <- function(pkgname) {
+    ## Mung suggested packages metadata to remove tab and newline characters
+    suggests <- packageDescription(pkgname)$Suggests
+    suggests <- gsub('\n', ' ', suggests)
+    suggests <- gsub('\t', '',  suggests)
+
+    unlist(strsplit(suggests, ', '))
+}
+
+
 ## Load packages for all possible processing options
-suggested <- unlist(strsplit(packageDescription(pkgname)$Suggests, ', '))
-invisible(sapply(suggested,
+invisible(sapply(suggestedPackages(),
                  function(pkgname) {
                      suppressPackageStartupMessages(
                          require(pkgname, character.only=TRUE))
